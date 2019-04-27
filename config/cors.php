34bbb5d1907591d13921bc12dfd124dbea169fe1
Jason <jasonlund@gmail.com>
2019-04-27 13:11:37 -0400

    add production server to cors

--- config/cors.php
+++ config/cors.php
@@ -13,7 +13,7 @@ return [
     */
    
     'supportsCredentials' => false,
-    'allowedOrigins' => [ 'http://localhost' ],
+    'allowedOrigins' => [ 'http://localhost', 'https://nexus.thepylonshow.com' ],
     'allowedOriginsPatterns' => [ '/localhost:\d/' ],
     'allowedHeaders' => ['*'],
     'allowedMethods' => ['*'],
