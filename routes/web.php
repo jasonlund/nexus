064d6c9e075b9185e421c51cd2bc2a9b1a57eeff
Jason <jasonlund@gmail.com>
2019-07-29 16:45:18 -0400

    wip

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,1 @@
 <?php
-
-Route::get('/test', function() {
-    throw new Exception('this is a test');
-});
-
-Route::get('/debug-sentry', function () {
-    throw new Exception('My first Sentry error!');
-});
