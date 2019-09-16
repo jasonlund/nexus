a42a3c0c0281980a720d228a2e03a16f5822b881
Jason <jasonlund@gmail.com>
2019-09-16 17:54:55 -0400

    Revert "switch to spatie cors for proper corb support"
    This reverts commit 8d2abc4fbcb305dd2eaba0e948f70de0a467f1bf.

--- config/cors.php
+++ config/cors.php
@@ -3,59 +3,25 @@
 return [
 
     /*
-     * A cors profile determines which origins, methods, headers are allowed for
-     * a given requests. The `DefaultProfile` reads its configuration from this
-     * config file.
-     *
-     * You can easily create your own cors profile.
-     * More info: https://github.com/spatie/laravel-cors/#creating-your-own-cors-profile
-     */
-    'cors_profile' => Spatie\Cors\CorsProfile\DefaultProfile::class,
+    |--------------------------------------------------------------------------
+    | Laravel CORS
+    |--------------------------------------------------------------------------
+    |
+    | allowedOrigins, allowedHeaders and allowedMethods can be set to array('*')
+    | to accept any value.
+    |
+    */
 
-    /*
-     * This configuration is used by `DefaultProfile`.
-     */
-    'default_profile' => [
-
-        'allow_credentials' => false,
-
-        'allow_origins' => env('CORS_ALLOWED_ORIGINS')
+    'supportsCredentials' => false,
+    'allowedOrigins' => env('CORS_ALLOWED_ORIGINS')
         ? explode(',', env('CORS_ALLOWED_ORIGINS'))
         : ['https://nexus.thepylonshow.com'],
+    'allowedOriginsPatterns' => env('CORS_ALLOWED_ORIGINS_PATTERNS')
+        ? explode(',', env('CORS_ALLOWED_ORIGINS_PATTERNS'))
+        : [],
+    'allowedHeaders' => ['*'],
+    'allowedMethods' => ['*'],
+    'exposedHeaders' => ['Authorization'],
+    'maxAge' => 0,
 
-        'allow_methods' => [
-            'POST',
-            'GET',
-            'OPTIONS',
-            'PUT',
-            'PATCH',
-            'DELETE',
-        ],
-
-        'allow_headers' => [
-            'Content-Type',
-            'X-Auth-Token',
-            'Origin',
-            'Authorization',
-        ],
-
-        'expose_headers' => [
-            'Cache-Control',
-            'Content-Language',
-            'Content-Type',
-            'Expires',
-            'Last-Modified',
-            'Pragma',
-        ],
-
-        'forbidden_response' => [
-            'message' => 'Forbidden (cors).',
-            'status' => 403,
-        ],
-
-        /*
-         * Preflight request will respond with value for the max age header.
-         */
-        'max_age' => 60 * 60 * 24,
-    ],
 ];
