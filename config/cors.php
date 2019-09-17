9a09af04dea0024c08f468ecaab27db7fc198770
Jason <jasonlund@gmail.com>
2019-09-17 10:56:28 -0400

    Revert "please work"
    This reverts commit de455ec649bd075fc703a3919db60c37df48377e.

--- config/cors.php
+++ config/cors.php
@@ -1,62 +1,23 @@
 <?php
-
 return [
-
     /*
-     * A cors profile determines which origins, methods, headers are allowed for
-     * a given requests. The `DefaultProfile` reads its configuration from this
-     * config file.
-     *
-     * You can easily create your own cors profile.
-     * More info: https://github.com/spatie/laravel-cors/#creating-your-own-cors-profile
-     */
-    'cors_profile' => Spatie\Cors\CorsProfile\DefaultProfile::class,
-
-    /*
-     * This configuration is used by `DefaultProfile`.
-     */
-    'default_profile' => [
-
-        'allow_credentials' => false,
-
-        'allow_origins' => env('CORS_ALLOWED_ORIGINS')
-            ? explode(',', env('CORS_ALLOWED_ORIGINS'))
-            : ['https://nexus.thepylonshow.com'],
-
-        'allow_methods' => [
-            'POST',
-            'GET',
-            'OPTIONS',
-            'PUT',
-            'PATCH',
-            'DELETE',
-        ],
-
-        'allow_headers' => [
-            'Content-Type',
-            'X-Auth-Token',
-            'Origin',
-            'Authorization',
-        ],
-
-        'expose_headers' => [
-            'Cache-Control',
-            'Content-Language',
-            'Content-Type',
-            'Expires',
-            'Last-Modified',
-            'Pragma',
-            'Authorization',
-        ],
-
-        'forbidden_response' => [
-            'message' => 'Forbidden (cors).',
-            'status' => 403,
-        ],
-
-        /*
-         * Preflight request will respond with value for the max age header.
-         */
-        'max_age' => 60 * 60 * 24,
-    ],
+    |--------------------------------------------------------------------------
+    | Laravel CORS
+    |--------------------------------------------------------------------------
+    |
+    | allowedOrigins, allowedHeaders and allowedMethods can be set to array('*')
+    | to accept any value.
+    |
+    */
+    'supportsCredentials' => false,
+    'allowedOrigins' => env('CORS_ALLOWED_ORIGINS')
+        ? explode(',', env('CORS_ALLOWED_ORIGINS'))
+        : ['https://nexus.thepylonshow.com'],
+    'allowedOriginsPatterns' => env('CORS_ALLOWED_ORIGINS_PATTERNS')
+        ? explode(',', env('CORS_ALLOWED_ORIGINS_PATTERNS'))
+        : [],
+    'allowedHeaders' => ['*'],
+    'allowedMethods' => ['*'],
+    'exposedHeaders' => ['Authorization'],
+    'maxAge' => 0,
 ];
